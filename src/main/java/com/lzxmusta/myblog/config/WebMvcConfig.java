dce6e8e1dffe322012700f01d7e7be11cec28ca8
lzxmusta <1244453574@qq.com>
2022-11-01 22:14:26 +0800

    Changes20221101-03

--- src/main/java/com/lzxmusta/myblog/config/WebMvcConfig.java
+++ src/main/java/com/lzxmusta/myblog/config/WebMvcConfig.java
@@ -41,7 +41,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(loginInterceptor)
                 .addPathPatterns("/test")
                 .addPathPatterns("/comments/create/change")
-//                .addPathPatterns("/articles/delArticle/{id}")
                 .addPathPatterns("/articles/publish");
     }
 
